09274c35c85b08dcf5b00503c9b57bdd301c19aa
dcorn068 <dcorn068@users.noreply.github.com>
2019-03-24 22:24:54 -0400

    fireworks!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,109 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+/*
+ * Calculates the angle between AB and the X axis
+ * A and B are points (ax,ay) and (bx,by)
+ */
+// https://stackoverflow.com/questions/42440588/find-inverse-tangent
+function getAngleDeg(ax, ay, bx, by) {
+  var angleRad = Math.atan((ay - by) / (ax - bx));
+  var angleDeg = (angleRad * 180) / Math.PI;
+
+  return angleDeg;
 }
+// distance between two points
+// https://stackoverflow.com/questions/20916953/get-distance-between-two-points-in-canvas
+const distance = ({ x1, x2, y1, y2 }) =>
+  Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+
+const AppStyles = styled.div`
+  overflow: hidden;
+  background-color: #282c34;
+  display: grid;
+  align-items: center;
+  height: 100vh;
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  position: relative;
+  .fireworkLauncher {
+    height: 30px;
+    width: 0;
+    border: 10px solid cornflowerblue;
+    background: tomato;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    z-index: 999;
+  }
+  .fireworks {
+    z-index: 1;
+    content: '';
+    position: absolute;
+    border: 2px dashed red;
+    margin-left: -2px;
+    margin-top: 40px;
+    width: 0;
+  }
+`;
+
+const App = () => {
+  const [coords, setCoords] = React.useState({ x: 0, y: 0 });
+  const [firing, setFiring] = React.useState(false);
+  const launcherCoords = { x: window.innerWidth / 2, y: window.innerHeight };
+
+  const angleDeg = getAngleDeg(
+    coords.x,
+    coords.y,
+    launcherCoords.x,
+    launcherCoords.y,
+  );
+
+  // angleDeg ranges from 0 to 90 (vertical) then flips to -90 to 0
+  const transform = `rotate(${
+    // angleDeg
+    angleDeg < 0 ? angleDeg + 270 : angleDeg + 90
+  }deg)`;
+
+  const handleMouseMove = event => {
+    setCoords({ x: event.pageX, y: event.pageY });
+  };
+
+  const handleMouseDown = event => {
+    setFiring(!firing);
+  };
+
+  const FireworksStyles = styled.div`
+    height: ${distance({
+      x1: coords.x,
+      x2: launcherCoords.x,
+      y1: coords.y,
+      y2: launcherCoords.y,
+    }) - 50}px;
+  `;
+
+  return (
+    <AppStyles onMouseMove={handleMouseMove} onMouseDown={handleMouseDown}>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div
+        className={`fireworkLauncher ${firing ? 'firing' : ''}`}
+        style={{ transform: transform }}
+      >
+        {firing && <FireworksStyles className="fireworks" />}
+      </div>
+    </AppStyles>
+  );
+};
 
 export default App;
